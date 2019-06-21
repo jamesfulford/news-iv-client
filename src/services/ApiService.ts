7a062d12a93a26398bf0915d9793b598f28d9e38
James Fulford <james.fulford@outlook.com>
2019-06-21 00:55:27 -0400

    refactor(api): Using error model

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,4 +1,5 @@
 import axios, { Method } from "axios";
+import { ErrorMessage } from "../models/error";
 
 export default class ApiService {
   public static async hit(method: Method, path: string, data?: any) {
@@ -8,7 +9,7 @@ export default class ApiService {
         data
       })).data;
     } catch (err) {
-      throw err.response.data.error;
+      throw (err.response.data as { error: ErrorMessage }).error;
     }
   }
 }
