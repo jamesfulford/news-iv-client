a28c502a8e9a678b11b5f9de47b790b911e923d4
James Fulford <james.fulford@outlook.com>
2019-06-21 00:53:46 -0400

    refactor(redux): Moving reducers to separate file, adding thunk helpers

--- src/store/reducers/errors/index.ts
+++ src/store/reducers/errors/index.ts
@@ -1,20 +1,19 @@
-import { ErrorType, ErrorAction } from './actions';
+import reducer from "./reducer";
+import { clearError, setError } from './actions';
+import { ErrorMessage } from "../../../models/error";
 
-export interface ErrorState {
-  message?: string;
+export default reducer;
+export * from "./actions";
+
+// Shortcuts
+export function errorClearer (dispatch: Function) {
+    return () => {
+        dispatch(clearError());
+    }
 }
 
-const initialState: ErrorState = {
-  message: undefined
-};
-
-export default (state: ErrorState = initialState, action: ErrorAction): ErrorState => {
-  switch (action.type) {
-    case ErrorType.SET:
-      return { ...state, message: action.error };
-    case ErrorType.CLEAR:
-      return { ...state, message: undefined };
-    default:
-      return state;
-  }
-};
+export function errorHandler (dispatch: Function) {
+    return (e: ErrorMessage) => {
+        dispatch(setError(e.message));
+    }
+}
